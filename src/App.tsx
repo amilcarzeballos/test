40d7cfa44330aca2b2e357f15e48daf20579339b
amilcar <amilcarzeballos1@gmail.com>
2021-12-28 10:13:37 -0400

    mi 3ercambio

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,9 @@ function App(): JSX.Element {
                 <button className="btn btn-succes btn-block mt-2">
                   Guardar
                 </button>
+                <button className="btn btn-succes btn-block mt-2">
+                  Guardar
+                </button>
               </form>
             </div>
           </div>
